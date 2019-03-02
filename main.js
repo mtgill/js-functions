ed87e084973a097db71fd3ba8ec9db2d51219487
Matt Gill <mtgill90@outlook.com>
2019-03-02 11:07:23 -0600

    name printer function

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('hi');
+//Matt Gill
+//Brittany Spears
+
+//const firstName = 'Matt';
+//const lastName = 'Gill';
+
+//console.log(firstName + " " + lastName);
+//console.log(`${firstName} ${lastName}`);
+
+//const firstName = 'Britteny';
+
+const namePrinter = (firstName, lastName) => {
+    console.log(`${firstName} ${lastName}`);
+};
+
+namePrinter('Zoe', 'Ames');
+namePrinter('John', 'Doe'); 
+
